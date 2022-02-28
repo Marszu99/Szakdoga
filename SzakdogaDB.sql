2b1bdde889e6f53b2379d833a0481cb7dffd52ed
Marcell Cseh <csehmarcell@yahoo.com>
2022-02-28 17:25:22 +0100

    Par uj tarolt eljaras

--- SzakdogaDB.sql
+++ SzakdogaDB.sql
@@ -119,7 +119,7 @@ CREATE TABLE `record` (
   KEY `fk_Record_User1_idx` (`User_idUser`) /*!80000 INVISIBLE */,
   CONSTRAINT `fk_Record_Task1` FOREIGN KEY (`Task_idTask`) REFERENCES `task` (`idTask`),
   CONSTRAINT `fk_Record_User1` FOREIGN KEY (`User_idUser`) REFERENCES `user` (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +128,7 @@ CREATE TABLE `record` (
 
 LOCK TABLES `record` WRITE;
 /*!40000 ALTER TABLE `record` DISABLE KEYS */;
-INSERT INTO `record` VALUES (4,'2021-12-15','Regisztacio',250,1,9,2),(5,'2021-12-11','Teszt',230,1,10,3),(6,'2021-12-18','Proba',220,1,9,5),(7,'2021-12-11',NULL,210,0,10,3),(8,'2021-12-12','Szép lett!',250,1,10,4),(9,'2021-12-18','Komment',120,1,9,2),(10,'2021-12-21',NULL,210,0,10,6),(11,'2021-12-21',NULL,210,0,10,6),(12,'2021-12-22','Kész',210,1,9,9),(13,'2021-12-23',NULL,210,0,10,6),(14,'2021-12-23',NULL,210,0,10,6),(15,'2022-02-08','',220,0,10,14);
+INSERT INTO `record` VALUES (4,'2021-12-15','Regisztacio',250,1,9,2),(5,'2021-12-11','Teszt',230,1,10,3),(6,'2021-12-18','Proba',220,1,9,5),(7,'2021-12-11',NULL,210,0,10,3),(8,'2021-12-12','Szép lett!',250,1,10,4),(9,'2021-12-18','Komment',120,1,9,2),(10,'2021-12-21',NULL,210,0,10,6),(11,'2021-12-21',NULL,210,0,10,6),(12,'2021-12-22','Kész',210,1,9,9),(13,'2021-12-23',NULL,210,0,10,6),(14,'2021-12-23',NULL,210,0,10,6),(15,'2022-02-08','',220,0,10,14),(16,'2022-02-17',NULL,210,0,10,14),(17,'2022-02-21',NULL,210,0,9,38),(18,'2022-02-22','dd',210,0,10,14),(19,'2022-02-22','ads',210,0,10,14),(20,'2022-02-22','d',210,0,9,45),(21,'2022-02-22','ss',210,0,9,5),(22,'2022-02-22','asd',210,0,9,18),(23,'2022-02-22','dd',240,0,9,18),(24,'2022-02-22','a',230,0,9,18),(25,'2022-02-22','d',210,0,9,18),(26,'2022-02-22','aaaaa',210,0,9,18),(27,'2022-02-23','a',230,0,9,18),(28,'2022-02-23','as',180,0,9,18),(29,'2022-02-23','a',210,0,9,18),(30,'2022-02-23','d',210,0,9,18),(31,'2022-02-23','asd',210,0,9,18),(32,'2022-02-23','dd',210,0,9,18);
 /*!40000 ALTER TABLE `record` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -171,7 +171,7 @@ CREATE TABLE `task` (
   PRIMARY KEY (`idTask`,`User_idUser`),
   KEY `fk_Task_User1_idx` (`User_idUser`),
   CONSTRAINT `fk_Task_User1` FOREIGN KEY (`User_idUser`) REFERENCES `user` (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -180,7 +180,7 @@ CREATE TABLE `task` (
 
 LOCK TABLES `task` WRITE;
 /*!40000 ALTER TABLE `task` DISABLE KEYS */;
-INSERT INTO `task` VALUES (1,'ddd',NULL,'2021-12-10','Created',0,'2021-12-09',1),(2,'Programozas','Mukodjon!','2022-07-22','InProgress',1,'2021-12-11',9),(3,'Teszteles','Unit tesztek','2022-03-18','Done',1,'2021-12-11',10),(4,'Design','Szep legyem!','2022-06-24','InProgress',1,'2021-12-11',10),(5,'DB','','2022-01-27','InProgress',1,'2021-12-11',9),(6,'Git','Nagyooooon hosssszuuuuu leiraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas','2022-07-31','InProgress',1,'2021-12-11',10),(7,'ASD',NULL,'2021-12-23','Created',0,'2021-12-22',10),(8,'ad','a','2020-01-01','Created',0,'2021-12-22',10),(9,'Wiki','','2021-12-23','Done',1,'2021-12-22',9),(10,'asd',NULL,'2021-12-24','Created',0,'2021-12-23',10),(11,'d',NULL,'2021-12-24','Created',0,'2021-12-23',9),(12,'a',NULL,'2021-12-24','Created',0,'2021-12-23',9),(13,'b',NULL,'2021-12-24','Created',0,'2021-12-23',9),(14,'AutomatákPDF','','2022-05-13','Created',1,'2021-12-23',10),(15,'d','dddd','2022-07-07','Created',0,'2022-01-27',10),(16,'ss','as','2022-01-28','Created',0,'2022-01-27',10),(17,'TestRefresh',NULL,'2022-08-26','Created',0,'2022-02-08',9),(18,'TestRefresh','','2022-08-27','Created',1,'2022-02-08',9),(19,'d',NULL,'2022-02-09','Created',0,'2022-02-08',9);
+INSERT INTO `task` VALUES (1,'ddd',NULL,'2021-12-10','Created',0,'2021-12-09',1),(2,'Programozas','Mukodjon!','2022-07-22','InProgress',1,'2021-12-11',9),(3,'Teszteles','Unit tesztek','2022-03-18','Done',1,'2021-12-11',10),(4,'Design','Szep legyem!','2022-06-24','InProgress',1,'2021-12-11',10),(5,'DB','','2023-05-26','InProgress',1,'2021-12-11',9),(6,'Git','Nagyooooon hosssszuuuuu leiraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas','2022-07-31','InProgress',1,'2021-12-11',10),(7,'ASD',NULL,'2021-12-23','Created',0,'2021-12-22',10),(8,'ad','a','2020-01-01','Created',0,'2021-12-22',10),(9,'Wiki','','2021-12-23','Done',1,'2021-12-22',9),(10,'asd',NULL,'2021-12-24','Created',0,'2021-12-23',10),(11,'d',NULL,'2021-12-24','Created',0,'2021-12-23',9),(12,'a',NULL,'2021-12-24','Created',0,'2021-12-23',9),(13,'b',NULL,'2021-12-24','Created',0,'2021-12-23',9),(14,'AutomatákPDF','','2022-05-13','Created',1,'2021-12-23',10),(15,'d','dddd','2022-07-07','Created',0,'2022-01-27',10),(16,'ss','as','2022-01-28','Created',0,'2022-01-27',10),(17,'TestRefresh',NULL,'2022-08-26','Created',0,'2022-02-08',9),(18,'TestRefresh','','2022-08-27','Created',1,'2022-02-08',9),(19,'d',NULL,'2022-02-09','Created',0,'2022-02-08',9),(20,'a',NULL,'2022-02-18','Created',0,'2022-02-17',9),(21,'l',NULL,'2022-02-18','Created',0,'2022-02-17',9),(22,'asd',NULL,'2022-03-04','Created',0,'2022-02-17',9),(23,'kk',NULL,'2022-02-18','Created',0,'2022-02-17',9),(24,'ll',NULL,'2022-02-25','Created',0,'2022-02-17',9),(25,'a',NULL,'2022-02-20','Created',0,'2022-02-19',9),(26,'d','','2022-02-20','Created',0,'2022-02-19',9),(27,'d',NULL,'2022-02-26','Created',0,'2022-02-19',9),(28,'a',NULL,'2022-03-04','Created',0,'2022-02-19',9),(29,'k',NULL,'2022-03-02','Created',0,'2022-02-19',9),(30,'l',NULL,'2022-03-02','Created',0,'2022-02-19',9),(31,'ll',NULL,'2022-03-03','Created',0,'2022-02-19',9),(32,'d',NULL,'2022-02-20','Created',0,'2022-02-19',9),(33,'d',NULL,'2022-03-05','Created',0,'2022-02-21',9),(34,'asd',NULL,'2022-03-11','Created',0,'2022-02-21',9),(35,'ddd',NULL,'2022-02-25','Created',0,'2022-02-21',9),(36,'s',NULL,'2022-02-22','Created',0,'2022-02-21',9),(37,'d',NULL,'2022-02-22','Created',0,'2022-02-21',9),(38,'a',NULL,'2022-02-22','Created',0,'2022-02-21',9),(39,'d',NULL,'2022-02-23','Created',0,'2022-02-22',9),(40,'dd',NULL,'2022-02-23','Created',0,'2022-02-22',9),(41,'dddd',NULL,'2022-02-23','Created',0,'2022-02-22',9),(42,'k',NULL,'2022-02-23','Created',0,'2022-02-22',9),(44,'l','k','2022-02-23','Created',0,'2022-02-22',9),(45,'asd',NULL,'2022-02-23','Created',0,'2022-02-22',9),(46,'ss',NULL,'2022-02-24','Created',0,'2022-02-23',9),(47,'asd','dd','2022-02-24','Created',0,'2022-02-23',9);
 /*!40000 ALTER TABLE `task` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -225,7 +225,7 @@ CREATE TABLE `user` (
   KEY `fk_User_Company1_idx` (`StatusDelete`),
   KEY `fk_User_Company1_idx1` (`Company_idCompany`),
   CONSTRAINT `fk_User_Company1` FOREIGN KEY (`Company_idCompany`) REFERENCES `company` (`idCompany`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -234,7 +234,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'asd','asd','asd','asd','asd','76876',1,0,1),(2,'Marszi99','Password','Cseh','Marci','csehmarcell@yahoo.com','06898776778',1,0,2),(3,'Marszi','Password','Marci','Cseh','csehmarcell@yahoo.com','06876876378',1,0,3),(4,'Marszu99','Password','Marcell','Cseh','csehmarcell@yahoo.com','06987394827',1,0,4),(5,'CsehHedvig','nfq3XGTxXP',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,4),(6,'CsehMarci','Ht7TpfQDkr',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,4),(7,'csehlaszlo','lacko12','aaaa','aaaa','csehmarcell@yahoo.com','36302988033',1,0,5),(8,'KovacsGeza','ObPZ0BmJoK',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,5),(9,'Marszu99','Password','Marcell','Cseh','csehmarcell@yahoo.com','06703070041',1,1,6),(10,'Username','Password','Laszlo','Cseh','csehmarcell@yahoo.com','06704009188',0,1,6),(11,'CsehMarcell','Password','Cseh','Marcell','csehmarcell@yahoo.com','+367088788666',0,0,6),(12,'asdddd','od5575qs1D',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,6),(13,'DeleteTeszt','DeleteTeszt',NULL,NULL,'DeleteTeszt',NULL,0,0,6),(14,'Username1','IdlUkA5G6W',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,6),(15,'Username1','1JiWgX9o5Z',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,6),(16,'Username1','AFaYlJnION','asda','asdd','csehmarcell@yahoo.com','06777878788',0,0,6),(17,'Username1','l5MAGzOgsr','asdasd','asd','csehmarcell@yahoo.com','06789877799',0,0,6),(18,'Username1','bo3JzWWbr2','asdd','asdd','csehmarcell@yahoo.com','0678877997',0,0,6),(19,'Username1','PqcE56V3x4','asdd','asdasd','csehmarcell@yahoo.com','0678778999',0,0,6),(20,'Username1','EhhmAJZAKi','asd','aasd','csehmarcell@yahoo.com','0678799777',0,0,6),(21,'Username1','yOsc4bSvx0','asdd','asdd','csehmarcell@yahoo.com','0670987987',0,0,6),(22,'Username1','rqwWWX0GDg','asdd','asdd','csehmarcell@yahoo.com','06709878979',0,0,6),(23,'Username1','n1Po55bbXH','asd','asd','csehmarcell@yahoo.com','0670876878',0,0,6);
+INSERT INTO `user` VALUES (1,'asd','asd','asd','asd','asd','76876',1,0,1),(2,'Marszi99','Password','Cseh','Marci','csehmarcell@yahoo.com','06898776778',1,0,2),(3,'Marszi','Password','Marci','Cseh','csehmarcell@yahoo.com','06876876378',1,0,3),(4,'Marszu99','Password','Marcell','Cseh','csehmarcell@yahoo.com','06987394827',1,0,4),(5,'CsehHedvig','nfq3XGTxXP',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,4),(6,'CsehMarci','Ht7TpfQDkr',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,4),(7,'csehlaszlo','lacko12','aaaa','aaaa','csehmarcell@yahoo.com','36302988033',1,0,5),(8,'KovacsGeza','ObPZ0BmJoK',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,5),(9,'Marszu99','Password','Marcell','Cseh','csehmarcell@yahoo.com','06703070041',1,1,6),(10,'Username','Password','Laszlo','Cseh','csehmarcell@yahoo.com','06704009188',0,1,6),(11,'CsehMarcell','Password','Cseh','Marcell','csehmarcell@yahoo.com','+367088788666',0,0,6),(12,'asdddd','od5575qs1D',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,6),(13,'DeleteTeszt','DeleteTeszt',NULL,NULL,'DeleteTeszt',NULL,0,0,6),(14,'Username1','IdlUkA5G6W',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,6),(15,'Username1','1JiWgX9o5Z',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,6),(16,'Username1','AFaYlJnION','asda','asdd','csehmarcell@yahoo.com','06777878788',0,0,6),(17,'Username1','l5MAGzOgsr','asdasd','asd','csehmarcell@yahoo.com','06789877799',0,0,6),(18,'Username1','bo3JzWWbr2','asdd','asdd','csehmarcell@yahoo.com','0678877997',0,0,6),(19,'Username1','PqcE56V3x4','asdd','asdasd','csehmarcell@yahoo.com','0678778999',0,0,6),(20,'Username1','EhhmAJZAKi','asd','aasd','csehmarcell@yahoo.com','0678799777',0,0,6),(21,'Username1','yOsc4bSvx0','asdd','asdd','csehmarcell@yahoo.com','0670987987',0,0,6),(22,'Username1','rqwWWX0GDg','asdd','asdd','csehmarcell@yahoo.com','06709878979',0,0,6),(23,'Username1','n1Po55bbXH','asd','asd','csehmarcell@yahoo.com','0670876878',0,0,6),(24,'Username1','jDnFJbLRfI',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,6),(25,'asddddd','h6XACndJwz',NULL,NULL,'csehmarcell@yahoo.com',NULL,0,0,6);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -585,6 +585,25 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `GetTaskByID` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetTaskByID`(IN taskid INT)
+BEGIN
+SELECT * FROM taskview WHERE taskview.idTask=taskid;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `GetTaskNotificationsForAdmin` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -920,4 +939,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-02-11 17:33:47
+-- Dump completed on 2022-02-28 17:23:07
